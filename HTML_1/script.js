6283af35cdfee9e06d9242ca06c86cda2328b6bc
Kuir Juach Kuir Thuch <127403871+kuir-juach@users.noreply.github.com>
2024-03-08 14:55:02 +0200

    Delete HTML_1

--- HTML_1/script.js
+++
@@ -1,7 +0,0 @@
-var typed = new Typed(".multiple-text",{
-    strings: ["Frontend Developer", "Python Developer", "Transcriptionalist","Data Analyst", "Microsoft Suits", "Typist"],
-    typeSpeed: 80,
-    backspeed: 100,
-    backDelay: 1000,
-    loop: true
-})
