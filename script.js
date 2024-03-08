0ce0152834f7dc359956b610c59d5be6fcc6b594
Kuir Juach Kuir Thuch <127403871+kuir-juach@users.noreply.github.com>
2024-03-08 14:56:27 +0200

    updated

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Frontend Developer", "Python Developer", "Transcriptionalist","Data Analyst", "Microsoft Suits", "Typist"],
+    typeSpeed: 80,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+})
